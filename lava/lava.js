1e254ca216f059f61d00725d51727334f8d674c8
dmgardiner25 <dmgardiner25@gmail.com>
2017-10-04 16:11:56 -0500

    Simple p5.js script that makes some sweet lava

---
+++ lava/lava.js
@@ -0,0 +1,97 @@
+var executed = false;
+var num_spots = 50;
+var spots = new Array(num_spots);
+var taken_x = new Array(num_spots);
+var taken_y = new Array(num_spots);
+var spots_made = 0;
+
+class lava_spot {
+  constructor() {
+    this.spot_x = Math.floor(Math.random() * 395);
+    this.spot_y = Math.floor(Math.random() * 95) + 300;
+    this.orig_y = this.spot_y;
+    this.color = Math.floor(Math.random() * 100) + 101;
+    this.color_dir = Math.floor(Math.random() * 2) + 1;
+    this.gravity = (Math.random() * (0.2 - 0.1) + 0.1);
+    this.gravity_speed = Math.floor(Math.random() * 5) + 5;
+    this.down = false;
+    this.stop = false;
+    this.jumping = false;
+  }
+  place() {
+    fill(255, this.color, 15);
+    rect(this.spot_x, this.spot_y, 5, 5);
+  }
+  re_draw() {
+    if (this.color >= 200)
+      this.color_dir *= -1;
+    else if (this.color <= 100)
+      this.color_dir *= -1;
+    fill(255, this.color += this.color_dir, 15);
+    rect(this.spot_x, this.spot_y, 5, 5);
+  }
+  jump() {
+    noStroke();
+    this.jumping = true;
+    if(!this.stop) {
+        if(this.down) {
+            this.down = true;
+            this.gravity_speed += this.gravity;
+            this.spot_y += this.gravity_speed;
+          if(this.spot_y >= this.orig_y)  
+            this.stop = true;
+        } else {
+            this.down = false;
+            this.gravity_speed -= this.gravity;
+            this.spot_y -= this.gravity_speed;
+            if(this.gravity_speed <= 0)
+                this.down = true;
+        }
+    }   
+    if(this.stop)
+      this.jumping = false;
+    this.re_draw();
+  }
+  get curr_y() { 
+    return this.spot_y; 
+  }
+  get start_y() {
+    return this.orig_y;
+  }
+  get is_jumping() {
+    return this.jumping;
+  }
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function make_spots(num) {
+  noStroke();
+  if (!executed) {
+    executed = true;
+    for (var i = 0; i < num; i++) {
+      spots[i] = new lava_spot();
+      spots[i].place();
+    }
+  }
+  for (var i = 0; i < num; i++) {
+    if(!spots[i].is_jumping && Math.floor(Math.random() * 1000) === 0)
+      spots[i].jump();
+    else if(spots[i].is_jumping)
+      spots[i].jump();
+    else
+      spots[i].re_draw();
+  }
+  print(spots[0].y_pos);
+}
+
+function draw() {
+  background(220);
+
+  // Lava
+  fill(255, 79, 15);
+  rect(0, 300, 400, 100);
+  make_spots(num_spots);
+}
